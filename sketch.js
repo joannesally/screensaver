9eb8b6a69062d73d93accf9671a53ee7235f8a65
joannesally <55815221+joannesally@users.noreply.github.com>
2019-10-08 08:54:33 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,14 +1,28 @@
-sketch.js
+
+{let totalPts = 100;
+let steps = totalPts + 3;
 
 function setup() {
-	
-	createCanvas(400, 300)
-	rect(0, 0, 400, 300)
-
-
-
-
-
-
-
+  createCanvas(1400, 700);
+  stroke(255, 255, 102);
+  strokeWeight(3)
+  frameRate(1.5);
 }
+
+function draw() {
+  background(0, 0, 102);
+  let rand = 20;
+  for (let i = 2; i < steps; i++) {
+    point((width / steps) * i, height / 2 + random(-rand, rand));
+    rand += random(-100, 110);}}
+  }
+
+//  {let totalPts = 200;
+// let steps = totalPts + 1;
+
+// function setup() {
+//   createCanvas(720, 1400);
+//   stroke(255, 255, 102);
+//   strokeWeight(7)
+//   frameRate(2);
+// } 
